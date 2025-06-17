34f22d5626ed344c5f587422d3058032208b8cee
Chase Williams <a.williams.chase@gmail.com>
2025-06-16 20:10:21 -0400

    gradio app init

---
+++ app.py
@@ -0,0 +1,84 @@
+import os
+from authlib.integrations.starlette_client import OAuth, OAuthError
+from fastapi import FastAPI, Depends, Request
+from starlette.config import Config
+from starlette.responses import RedirectResponse
+from starlette.middleware.sessions import SessionMiddleware
+import uvicorn
+import gradio as gr
+
+app = FastAPI()
+
+# Replace these with your own Spotify OAuth settings
+SPOTIFY_CLIENT_ID = "c23563670ff943438fdc616383e9f0ea"
+SPOTIFY_CLIENT_SECRET = "08e420d130d94312a20123663db0ec25"
+SECRET_KEY = os.environ.get('SECRET_KEY') or "a_very_secret_key"
+
+config_data = {'SPOTIFY_CLIENT_ID': SPOTIFY_CLIENT_ID, 'SPOTIFY_CLIENT_SECRET': SPOTIFY_CLIENT_SECRET}
+starlette_config = Config(environ=config_data)
+oauth = OAuth(starlette_config)
+oauth.register(
+    name='spotify',
+    client_id=SPOTIFY_CLIENT_ID,
+    client_secret=SPOTIFY_CLIENT_SECRET,
+    access_token_url='https://accounts.spotify.com/api/token',
+    authorize_url='https://accounts.spotify.com/authorize',
+    api_base_url='https://api.spotify.com/v1/',
+    client_kwargs={'scope': 'user-read-private, user-library-read'},  # Adjust scope as needed
+)
+
+app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
+
+# Dependency to get the current user
+def get_user(request: Request):
+    user = request.session.get('user')
+    if user:
+        return user['display_name']
+    return None
+
+@app.get('/')
+def public(user: dict = Depends(get_user)):
+    if user:
+        return RedirectResponse(url='/gradio')
+    else:
+        return RedirectResponse(url='/login-demo')
+
+@app.route('/logout')
+async def logout(request: Request):
+    request.session.pop('user', None)
+    return RedirectResponse(url='/')
+
+@app.route('/login')
+async def login(request: Request):
+    redirect_uri = request.url_for('auth')
+    return await oauth.spotify.authorize_redirect(request, redirect_uri)
+
+@app.route('/auth')
+async def auth(request: Request):
+    try:
+        token = await oauth.spotify.authorize_access_token(request)
+        resp = await oauth.spotify.get('me', token=token)
+        user_info = resp.json()
+    except OAuthError:
+        return RedirectResponse(url='/')
+
+    request.session['user'] = user_info
+    return RedirectResponse(url='/gradio')
+
+with gr.Blocks() as login_demo:
+    gr.Button("Login", link="/login")
+
+app = gr.mount_gradio_app(app, login_demo, path="/login-demo")
+
+def greet(request: gr.Request):
+    return f"Welcome to Gradio, {request.username}"
+
+with gr.Blocks() as main_demo:
+    m = gr.Markdown("Welcome to Gradio!")
+    gr.Button("Logout", link="/logout")
+    main_demo.load(greet, None, m)
+
+app = gr.mount_gradio_app(app, main_demo, path="/gradio", auth_dependency=get_user)
+
+if __name__ == '__main__':
+    uvicorn.run(app)
